c146ce254919bc505e97bab03aa3b54d075bd7e8
Nurdaulet002 <73096695+Nurdaulet002@users.noreply.github.com>
2023-08-18 17:25:45 +0600

    wdwd

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,4 @@
+from rest_framework.authtoken.models import Token
 from django.contrib import admin
 from .models import User, Industry, Company, FuncStructure, Speciality
 
@@ -6,3 +7,10 @@ admin.site.register(Industry)
 admin.site.register(Company)
 admin.site.register(FuncStructure)
 admin.site.register(Speciality)
+
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('key', 'user', 'created')
+    ordering = ('-created',)
